452db43966122167443ce9301d9b74a0d82dcd26
Dijkstra7 <rickr.dijkd@gmail.com>
2018-06-01 14:00:52 +0200

    Commiting the hard way part 3
    Scraped tweets and cleaned them
    
    Done:
    hyperlinks changed into <hyperlink>
    cleaned text
    added data_loader

---
+++ data/Load_Data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import csv
+
+
+def load_data(fname):
+    if fname[-4:] == ".csv":
+        fname = fname[:-4]
+    data = pd.read_csv("{}.csv".format(fname), error_bad_lines=False, quoting=csv.QUOTE_NONE)
+    print(data.head(20))
+    return data
+
+
+if __name__ == "__main__":
+    load_data("clean trump.csv")
